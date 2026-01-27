429aaae5e59a181f99a54aea554907255aacd6dd
alimuhammed90 <unlumuhammedali90@gmail.com>
2026-01-27 13:15:36 +0300

    Add new routes for nedenler and cozumler pages

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def ana_sayfa():
     return render_template("index.html")
 
+@app.route("/nedenler")
+def nedenler():
+    return render_template("nedenler.html")
+
+@app.route("/cozumler")
+def cozumler():
+    return render_template("cozumler.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
